397d6c3afea991cdce34ce3d02a25756cd7d0b4f
Dilara <74327986+dilaraizibuyuk@users.noreply.github.com>
2022-04-06 01:46:55 +0300

    WeatherForecast
    Html/Css/Javascript ~Hava Durumu Web Projesi

---
+++ WeatherForecastProject/script.js
@@ -0,0 +1,33 @@
+const url='https://api.openweathermap.org/data/2.5/'
+const key='817a4399b0d819b7cf6fd2a145975385'
+
+const setQuery=(e)=>{
+    if(e.keyCode=='13')
+    getResult(searchBar.value)
+}
+
+const getResult=(cityName)=>{
+let query= `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=tr`
+fetch(query)
+.then(weather=>{
+    return weather.json()
+})
+.then(displayResult)
+}
+
+const displayResult=(result) =>{
+let city=document.querySelector('.city')
+city.innerText=`${result.name},${result.sys.country}`
+
+let temp=document.querySelector('.temp')
+temp.innerText=`${Math.round(result.main.temp)}°C`
+
+let desc=document.querySelector('.desc')
+desc.innerText=result.weather[0].description
+
+let minmax=document.querySelector('.minmax')
+minmax.innerText=`${Math.round(result.main.temp_min)} / ${Math.round(result.main.temp_max)} `
+}
+
+const searchBar=document.getElementById('searchBar')
+searchBar.addEventListener('keypress',setQuery)
